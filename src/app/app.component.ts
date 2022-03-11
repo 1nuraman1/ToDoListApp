54c4bfb7444fec00b728323a6fc044a632fa854c
1nuraman1 <nursultanamanaliev24@gmail.com>
2022-03-11 12:57:03 +0600

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,42 @@
-import { Component } from '@angular/core';
+import { Component,  TemplateRef, Input, ElementRef } from '@angular/core';
 
+interface Task{
+ title: string,
+ is_canceled: boolean
+ 
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
+ 
 })
 export class AppComponent {
-  title = 'ToDoList';
+ tasks: Array<Task> = [];
 
-  list:any [] = [];
-  addTask(item:string) {
-  
-    this.list.push({id:this.list.length,name:item});
-    console.warn(this.list);
-  }
+ addTask(input: { value: string; }){
+   let value = input.value;
+   input.value = "";
+   this.tasks.push(
+     {
+       title: value,
+       is_canceled: false
+     });
+ }
 
-  deleteTask(id:number) {
-    this.list = this.list.filter(item => item.id!==id);
-  }
 
+ deleteTask(idx: number){
+   let do_delete = confirm("Are you sure to delete the task?");
+   if (do_delete){
+     this.tasks.splice(idx, 1);
+   }
+ }
+
+ editTask(idx: number){
+   let title = this.tasks[idx].title;
+   let result = prompt("Enter task", title);
+   if (result !== null && result !== ""){
+     this.tasks[idx].title = result;
+   }
+ }
 }
